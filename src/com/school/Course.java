c04f9ea66636fad6d73c242b03317da70023a8ff
Subhadeep-Samanta <subhadeepsamanta1535@gmail.com>
2025-08-05 11:13:09 +0530

    work done for part_03

--- src/com/school/Course.java
+++ src/com/school/Course.java
@@ -1,15 +1,16 @@
 package com.school;
 
 public class Course {
-//    courseId`, `courseName`, `setDetails()`, and `displayDetails()
-    int courseId;
-    String courseName;
-    void setDetails(int courseId,String courseName){
-        this.courseId=courseId;
-        this.courseName=courseName;
-    }
-   void displayDetails(){
-       System.out.println("Course id : "+courseId+" Course name : "+courseName);
+    private static int nextCourseIdCounter = 101;
+    private int courseId;
+    private String courseName;
 
-   }
+    public Course(String courseName) {
+        this.courseId = nextCourseIdCounter++;
+        this.courseName = courseName;
+    }
+
+    public void displayDetails() {
+        System.out.println("Course id : C" + courseId + " Course name : " + courseName);
+    }
 }
--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -2,21 +2,22 @@ package com.school;
 
 public class Main {
     public static void main(String[] args) {
-        Student[] Students=new Student[2];
-        Students[0] = new Student();
-        Students[1] = new Student();
-        Students[0].setDetails(18,"Subhadeep");
-        Students[1].setDetails(19,"Suman");
-        for(int i=0;i<2;i++){
-            Students[i].display();
+        Student[] students = new Student[3];
+        students[0] = new Student("Subhadeep");
+        students[1] = new Student("Suman");
+        students[2] = new Student("Sagar");
+
+        for (Student student : students) {
+            student.display();
         }
-Course[] courses=new Course[2];
-        courses[0]=new Course();
-        courses[1]=new Course();
-courses[0].setDetails(1,"OOP");
-courses[1].setDetails(2,"DBMS");
-        for(int i=0;i<2;i++){
-            courses[i].displayDetails();
+
+        Course[] courses = new Course[3];
+        courses[0] = new Course("OOP");
+        courses[1] = new Course("DBMS");
+        courses[2] = new Course("Networks");
+
+        for (Course course : courses) {
+            course.displayDetails();
         }
     }
 }
